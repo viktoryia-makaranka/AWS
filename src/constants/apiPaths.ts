8396dbfff59bd1a6dd70a2f99e5ee77245f8eaf3
Viktoryia_Makaranka <Viktoryia_Makaranka@epam.com>
2023-04-11 02:17:22 +0300

    [3] - task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
