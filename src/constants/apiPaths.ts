5633e870a4832e0887808432047916621a4a75dc
Viktoryia_Makaranka <Viktoryia_Makaranka@epam.com>
2023-05-15 01:03:03 +0300

    [5] - import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://gaxohhjk7b.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://xgajzulxpg.execute-api.us-east-1.amazonaws.com/dev",
 };
